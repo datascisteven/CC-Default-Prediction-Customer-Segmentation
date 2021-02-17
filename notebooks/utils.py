a5653f8317878448baa3f798d7c32ab3610704c1
Steven Yan <examsherpa@gmail.com>
2021-02-17 08:05:55 -0500

    update files

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,41 +1,45 @@
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, precision_recall_curve, roc_auc_score, classification_report, 
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, roc_curve, auc, average_precision_score
 
-def accuracy(y, y_hat):
-    y_y_hat = list(zip(y, y_hat))
-    tp = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 1])
-    tn = sum([1 for i in y_y_hat if i[0] == 0 and i[1] == 0])
-    return (tp + tn) / float(len(y_y_hat))
-
-def f1(y, y_hat):
-    precision_score = precision(y, y_hat)
-    recall_score = recall(y, y_hat)
-    numerator = precision_score * recall_score
-    denominator = precision_score + recall_score
-    return 2 * (numerator / denominator)
-
-def precision(y, y_hat):
-    y_y_hat = list(zip(y, y_hat))
-    tp = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 1])
-    fp = sum([1 for i in y_y_hat if i[0] == 0 and i[1] == 1])
-    return tp / float(tp + fp)
 
 def recall(y, y_hat):
-    # Your code here
     y_y_hat = list(zip(y, y_hat))
     tp = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 1])
     fn = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 0])
     return tp / float(tp + fn)
 
+def auc(X, y, model):
+    probs = model.predict_proba(X)[:,1] 
+    return roc_auc_score(y, probs)
+
+def aps(X, y, model):
+    probs = model.predict_proba(X)[:,1]
+    return average_precision_score(y, probs)
+
 def get_metrics(X_tr, y_tr, X_val, y_val, y_pred_tr, y_pred_val, model):
-    print('Training Accuracy: ', accuracy(y_tr, y_pred_tr))
-    print('Validation Accuracy: ', accuracy(y_val, y_pred_val))
-    print('Training F1 Score: ', f1(y_tr, y_pred_tr))
-    print('Validation F1 Score: ', f1(y_val, y_pred_val))
-    print('Training AUC Score: {}'.format(roc_auc_score(y_tr, model.predict_proba(X_tr)[:,1])))
-    print('Validation AUC Score: {}'.format(roc_auc_score(y_val, model.predict_proba(X_val)[:,1])))
-    print('Training Recall Score: ', recall(y_tr, y_pred_tr))
-    print('Validation Recall Score: ', recall(y_val, y_pred_val))
-    print('Training Precision Score: ', precision(y_tr, y_pred_tr))
-    print('Validation Precision Score: ', precision(y_val, y_pred_val))
+    ac_tr = accuracy_score(y_tr, y_pred_tr)
+    ac_val= accuracy_score(y_val, y_pred_val)
+    f1_tr = f1_score(y_tr, y_pred_tr)
+    f1_val = f1_score(y_val, y_pred_val)
+    au_tr = auc(X_tr, y_tr, model)
+    au_val = auc(X_val, y_val, model)
+    rc_tr = recall_score(y_tr, y_pred_tr)
+    rc_val = recall_score(y_val, y_pred_val)
+    pr_tr = precision_score(y_tr, y_pred_tr)
+    pr_val = precision_score(y_val, y_pred_val)
+
+    print('Training Accuracy: ', )
+    print('Validation Accuracy: ', )
+    print('Training F1 Score: ', )
+    print('Validation F1 Score: ', )
+    print('Training AUC Score: ', )
+    print('Validation AUC Score: ', )
+    print('Training Recall Score: ', )
+    print('Validation Recall Score: ', )
+    print('Training Precision Score: ', )
+    print('Validation Precision Score: ', )
     print('')
     print("Training Classification Report: ")
     print(classification_report(y_tr, y_pred_tr))
