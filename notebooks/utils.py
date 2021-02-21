d461f79bc97a15c32c3438fcc27f663902cf9737
Steven Yan <examsherpa@gmail.com>
2021-02-21 00:05:14 -0500

    update files

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, roc_curve, auc, average_precision_score, confusion_matrix, precision_recall_curve, roc_auc_score, classification_report
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, roc_curve, auc, average_precision_score, confusion_matrix, precision_recall_curve 
+from sklearn.metrics import roc_auc_score, classification_report, plot_precision_recall_curve
 
 def accuracy(y, y_hat):
     y_y_hat = list(zip(y, y_hat))
@@ -33,8 +34,16 @@ def auc(X, y, model):
     probs = model.predict_proba(X)[:,1] 
     return roc_auc_score(y, probs)
 
+def auc2(X, y, model):
+    probs = model.decision_function(X)
+    return roc_auc_score(y, probs)
+
 def aps(X, y, model):
     probs = model.predict_proba(X)[:,1]
+    return average_precision_score(y, probs)
+
+def aps2(X, y, model):
+    probs = model.decision_function(X)
     return average_precision_score(y, probs)
 
 def get_metrics(X_tr, y_tr, X_val, y_val, y_pred_tr, y_pred_val, model):
@@ -78,6 +87,47 @@ def get_metrics(X_tr, y_tr, X_val, y_val, y_pred_tr, y_pred_val, model):
     labels = np.asarray(labels).reshape(2,2)
     sns.heatmap(cnf, annot=labels, fmt='', cmap='Blues', annot_kws={'size':16})
 
+def get_metrics_2(X_tr, y_tr, X_val, y_val, y_pred_tr, y_pred_val, model):
+    ac_tr = accuracy_score(y_tr, y_pred_tr)
+    ac_val= accuracy_score(y_val, y_pred_val)
+    f1_tr = f1_score(y_tr, y_pred_tr)
+    f1_val = f1_score(y_val, y_pred_val)
+    au_tr = auc2(X_tr, y_tr, model)
+    au_val = auc2(X_val, y_val, model)
+    rc_tr = recall_score(y_tr, y_pred_tr)
+    rc_val = recall_score(y_val, y_pred_val)
+    pr_tr = precision_score(y_tr, y_pred_tr)
+    pr_val = precision_score(y_val, y_pred_val)
+    aps_tr = aps2(X_tr, y_tr, model)
+    aps_val = aps2(X_val, y_val, model)
+
+    print('Training Accuracy: ', ac_tr)
+    print('Validation Accuracy: ', ac_val)
+    print('Training F1 Score: ', f1_tr)
+    print('Validation F1 Score: ', f1_val)
+    print('Training AUC Score: ', au_tr)
+    print('Validation AUC Score: ', au_val)
+    print('Training Recall Score: ', rc_tr)
+    print('Validation Recall Score: ', rc_val)
+    print('Training Precision Score: ', pr_tr)
+    print('Validation Precision Score: ', pr_val)
+    print('Training Average Precision Score: ', aps_tr)
+    print('Validation Average Precision Score: ', aps_val)
+    print('')
+    print("Training Classification Report: ")
+    print(classification_report(y_tr, y_pred_tr))
+    print("")
+    print("Validation Classification Report: ")
+    print(classification_report(y_val, y_pred_val))
+    
+    cnf = confusion_matrix(y_val, y_pred_val)
+    group_names = ['TN','FP','FN','TP']
+    group_counts = ['{0:0.0f}'.format(value) for value in cnf.flatten()]
+    group_percentages = ['{0:.2%}'.format(value) for value in cnf.flatten()/np.sum(cnf)]
+    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cnf, annot=labels, fmt='', cmap='Blues', annot_kws={'size':16})
+
 def plot_feature_importances(X, model):
     n_features = X.shape[1]
     plt.figure(figsize=(8, 8))
@@ -85,3 +135,10 @@ def plot_feature_importances(X, model):
     plt.yticks(np.arange(n_features), X.columns.values)
     plt.xlabel("Feature Importance")
     plt.ylabel('Feature')
+
+
+def pr_curve(X, y, model):
+    y_score = model.decision_function(X) 
+    ap = average_precision_score(y, y_score)
+    disp = plot_precision_recall_curve(model, X, y)
+    disp.ax_.set_title('Precision-Recall Curve: AP={0:0.2f}'.format(ap))
